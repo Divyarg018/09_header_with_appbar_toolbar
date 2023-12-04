0bf57985c51d579552bcdd2979b7273930f34259
Divya R <kwrdivyaraj100@gmail.com>
2023-12-04 16:00:13 +0530

    Installing material ui packages

--- mui-tutorial/src/Test.jsx
+++ mui-tutorial/src/Test.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { AppBar, Button, ToolBar, Typography } from "@mui/material";
 
 function Test() {
-  return (
-    <div>Test</div>
-  )
+    return (
+        <div>
+            <AppBar>
+                <ToolBar>
+                    <Typography>Logo</Typography>
+                    <Button variant="contained" sx={{ marginLeft: "auto" }} color="warning">Hello</Button>
+                </ToolBar>
+            </AppBar>
+        </div>
+    )
 }
 
 export default Test;
