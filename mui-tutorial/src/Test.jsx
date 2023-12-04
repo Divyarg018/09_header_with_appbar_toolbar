0cb0d413a20d3207386ba68acf92c982faf2bd97
Divya R <kwrdivyaraj100@gmail.com>
2023-12-04 15:44:00 +0530

    Material UI select component

--- mui-tutorial/src/Test.jsx
+++ mui-tutorial/src/Test.jsx
@@ -1,8 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { TextField, Button, FormGroup, FormControlLabel, Checkbox, FormControl, InputLabel, Select, MenuItem } from '@mui/material';
 
 function Test() {
+    const [inputs, setInputs] = useState({
+        name: "",
+        email: "",
+        password: "",
+        subscribe: false,
+        age: 0
+    });
+    function handleChange(e) {
+        setInputs((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value
+        }))
+    }
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(inputs);
+    }
     return (
-        <div>Test</div>
+        <div>
+            <form style={{ display: "flex", flexDirection: "column" }} onSubmit={handleSubmit}>
+                <TextField name='name' value={inputs.name} onChange={handleChange} type={'text'} sx={{ margin: 3 }} placeholder='Name' variant='outlined' />
+                <TextField name='email' value={inputs.email} onChange={handleChange} type={'email'} sx={{ margin: 3 }} placeholder='Email' variant='standard' />
+                <TextField name='password' value={inputs.password} onChange={handleChange} type={'password'} sx={{ margin: 3 }} placeholder='Password' variant='filled' />
+
+                <FormGroup>
+                    <FormControlLabel control={<Checkbox onChange={() => setInputs((prev) => ({ ...prev, subscribe: !inputs.subscribe, }))} />} label="Subscribe To Newsletter" />
+                </FormGroup>
+
+                <FormControl fullWidth>
+                    <InputLabel >Age</InputLabel>
+                    <Select name="age"
+
+                        value={inputs.age}
+                        label="Age"
+                        onChange={handleChange}
+                    >
+                        <MenuItem value={10}>Ten</MenuItem>
+                        <MenuItem value={20}>Twenty</MenuItem>
+                        <MenuItem value={30}>Thirty</MenuItem>
+                    </Select>
+                </FormControl>
+                <Button type='submit'>Submit</Button>
+            </form>
+        </div>
     )
 }
 
